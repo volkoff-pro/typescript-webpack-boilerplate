dd9b7929a4bb8bac55805e07d830ec1a3b7d9551
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2018-10-18 18:30:44 +0400

    Init

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+document.write('Output from TypeScript File');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    chunkFilename: 'static/js/[name].chunk.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: require.resolve('ts-loader')
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
